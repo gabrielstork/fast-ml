88ae29d17b4792f8d7776c11c04a8647a0f261ba
storkdeveloper@gmail.com <storkdeveloper@gmail.com>
2021-07-30 14:57:43 -0300

    minor updates

--- fast-ml/toplevels.py
+++ fast-ml/toplevels.py
@@ -8,17 +8,17 @@ class Steps(tk.Toplevel):
         super().__init__(master)
 
         self.title(f'Currently working with {title}')
-        self.geometry('805x500')
-        self.resizable(0, 0)
+        self.geometry('1080x720')
 
-        self.split = frames.Split(self)
-        self.split.pack(fill='both', expand=True, side='left')
+        self.data = frames.Data(self)
+        self.data.pack(fill='both', expand=True, side='left')
+        self.data.display_data(dir)
 
         self.algorithm = frames.Algorithm(self)
         self.algorithm.pack(fill='both', expand=True, side='left')
 
         self.result = frames.Result(self)
-        self.result.pack(fill='both', expand=True, side='right')
+        self.result.pack(fill='both', expand=True, side='left')
 
         self.focus_force()
         self.grab_set()
